22e7bf31188afed774873c7d7986683b3a54c3fa
Matifridman <82188442+Matifridman@users.noreply.github.com>
2022-04-07 10:35:12 -0300

    Consigna agregada

--- Assets/Ejercicio01.cs
+++ Assets/Ejercicio01.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Ejercicio02.cs
+++ Assets/Ejercicio02.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿//2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos
+//y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+//Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
